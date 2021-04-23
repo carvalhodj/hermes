3d4ce1c060dbe0e1ac234d1ade4d52eb64a118c3
Daniel Carvalho <danieljosecarvalho@gmail.com>
2021-04-23 08:36:08 -0300

    - Bluetooth test connection;

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -10,6 +10,8 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final List<BluetoothDevice> devicesList = <BluetoothDevice>[];
+  BluetoothDevice _connectedDevice;
+  List<BluetoothService> _services;
 
   _addDeviceToList(final BluetoothDevice device) {
     if (!devicesList.contains(device)) {
@@ -21,6 +23,8 @@ class _HomeScreenState extends State<HomeScreen> {
 
   ListView _buildListViewOfDevices() {
     List<Container> containers = <Container>[];
+    final _formKey = GlobalKey<FormState>();
+
     for (BluetoothDevice device in devicesList) {
       containers.add(Container(
         height: 50,
@@ -41,7 +45,82 @@ class _HomeScreenState extends State<HomeScreen> {
                     backgroundColor:
                         MaterialStateProperty.all<Color>(Colors.blueGrey[800])),
                 onPressed: () {
-                  print('Conectando...');
+                  setState(() async {
+                    widget.flutterBlue.stopScan();
+                    try {
+                      await device.connect();
+                    } catch (e) {
+                      if (e.code != 'already_connected') {
+                        throw e;
+                      }
+                    } finally {
+                      _services = await device.discoverServices();
+                    }
+                    _connectedDevice = device;
+                  });
+                  // showDialog(
+                  //     context: context,
+                  //     builder: (BuildContext context) {
+                  //       return AlertDialog(
+                  //         content: Stack(
+                  //           clipBehavior: Clip.antiAlias,
+                  //           children: <Widget>[
+                  //             Positioned(
+                  //               right: -40.0,
+                  //               top: -40.0,
+                  //               child: InkResponse(
+                  //                 onTap: () {
+                  //                   Navigator.of(context).pop();
+                  //                 },
+                  //                 child: CircleAvatar(
+                  //                   child: Icon(Icons.close),
+                  //                   backgroundColor: Colors.orange[700],
+                  //                 ),
+                  //               ),
+                  //             ),
+                  //             Form(
+                  //               key: _formKey,
+                  //               child: Column(
+                  //                 mainAxisSize: MainAxisSize.min,
+                  //                 children: <Widget>[
+                  //                   Padding(
+                  //                     padding: EdgeInsets.all(8.0),
+                  //                     child: TextFormField(),
+                  //                   ),
+                  //                   Padding(
+                  //                     padding: EdgeInsets.all(8.0),
+                  //                     child: TextFormField(),
+                  //                   ),
+                  //                   Padding(
+                  //                     padding: EdgeInsets.all(8.0),
+                  //                     child: DropdownButtonFormField<int>(
+                  //                       items: [1, 2, 3, 4, 5]
+                  //                           .map((label) => DropdownMenuItem(
+                  //                                 child: Text(label.toString()),
+                  //                                 value: label,
+                  //                               ))
+                  //                           .toList(),
+                  //                     ),
+                  //                   ),
+                  //                   Padding(
+                  //                     padding: const EdgeInsets.all(8.0),
+                  //                     child: ElevatedButton(
+                  //                       child: Text("Send Packet"),
+                  //                       onPressed: () {
+                  //                         if (_formKey.currentState
+                  //                             .validate()) {
+                  //                           _formKey.currentState.save();
+                  //                         }
+                  //                       },
+                  //                     ),
+                  //                   )
+                  //                 ],
+                  //               ),
+                  //             ),
+                  //           ],
+                  //         ),
+                  //       );
+                  //     });
                 },
                 child: Text(
                   'Connect',
@@ -80,6 +159,115 @@ class _HomeScreenState extends State<HomeScreen> {
     widget.flutterBlue.startScan();
   }
 
+  ListView _buildView() {
+    if (_connectedDevice != null) {
+      return _buildConnectDeviceView();
+    }
+    return _buildListViewOfDevices();
+  }
+
+  List<ButtonTheme> _buildReadWriteNotifyButton(
+      BluetoothCharacteristic characteristic) {
+    List<ButtonTheme> buttons = new List<ButtonTheme>();
+
+    if (characteristic.properties.read) {
+      buttons.add(
+        ButtonTheme(
+          minWidth: 10,
+          height: 20,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 4),
+            child: RaisedButton(
+              color: Colors.blue,
+              child: Text('READ', style: TextStyle(color: Colors.white)),
+              onPressed: () {},
+            ),
+          ),
+        ),
+      );
+    }
+    if (characteristic.properties.write) {
+      buttons.add(
+        ButtonTheme(
+          minWidth: 10,
+          height: 20,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 4),
+            child: RaisedButton(
+              child: Text('WRITE', style: TextStyle(color: Colors.white)),
+              onPressed: () {},
+            ),
+          ),
+        ),
+      );
+    }
+    if (characteristic.properties.notify) {
+      buttons.add(
+        ButtonTheme(
+          minWidth: 10,
+          height: 20,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 4),
+            child: RaisedButton(
+              child: Text('NOTIFY', style: TextStyle(color: Colors.white)),
+              onPressed: () {},
+            ),
+          ),
+        ),
+      );
+    }
+
+    return buttons;
+  }
+
+  ListView _buildConnectDeviceView() {
+    List<Container> containers = new List<Container>();
+
+    for (BluetoothService service in _services) {
+      List<Widget> characteristicsWidget = new List<Widget>();
+      for (BluetoothCharacteristic characteristic in service.characteristics) {
+        characteristic.value.listen((value) {
+          print(value);
+        });
+        characteristicsWidget.add(
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Text(characteristic.uuid.toString(),
+                        style: TextStyle(fontWeight: FontWeight.bold)),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    ..._buildReadWriteNotifyButton(characteristic),
+                  ],
+                ),
+                Divider(),
+              ],
+            ),
+          ),
+        );
+      }
+      containers.add(
+        Container(
+          child: ExpansionTile(
+              title: Text(service.uuid.toString()),
+              children: characteristicsWidget),
+        ),
+      );
+    }
+
+    return ListView(
+      padding: const EdgeInsets.all(8),
+      children: <Widget>[
+        ...containers,
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -88,7 +276,7 @@ class _HomeScreenState extends State<HomeScreen> {
         backgroundColor: Colors.blueGrey[800],
       ),
       backgroundColor: Colors.blueGrey,
-      body: _buildListViewOfDevices(),
+      body: _buildView(),
     );
   }
 }
