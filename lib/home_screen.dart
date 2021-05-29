1121ab7484b40bfaedf5af9388c0af28d4c0e5fd
Daniel Carvalho <danieljosecarvalho@gmail.com>
2021-05-29 15:41:04 -0300

    - Remove unnecessary parts of code; - Reformat the connected device screen;

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_blue/flutter_blue.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -169,113 +170,125 @@ class _HomeScreenState extends State<HomeScreen> {
     return _buildListViewOfDevices();
   }
 
-  List<ButtonTheme> _buildReadWriteNotifyButton(
+  // List<ButtonTheme> _buildReadWriteNotifyButton(
+  List<Widget> _buildReadWriteNotifyButton(
       BluetoothCharacteristic characteristic) {
-    List<ButtonTheme> buttons = new List<ButtonTheme>();
+    // List<ButtonTheme> buttons = new List<ButtonTheme>();
+    // List<ButtonTheme> buttons = [];
+    List<Widget> buttons = [];
 
-    if (characteristic.properties.read) {
-      buttons.add(
-        ButtonTheme(
-          minWidth: 10,
-          height: 20,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 4),
-            child: RaisedButton(
-              color: Colors.blue,
-              child: Text('READ', style: TextStyle(color: Colors.white)),
-              onPressed: () {},
-            ),
-          ),
-        ),
-      );
-    }
+    // if (characteristic.properties.read) {
+    //   buttons.add(
+    //     ButtonTheme(
+    //       minWidth: 10,
+    //       height: 20,
+    //       child: Padding(
+    //         padding: const EdgeInsets.symmetric(horizontal: 4),
+    //         child: ElevatedButton(
+    //           style: ButtonStyle(
+    //               backgroundColor:
+    //                   MaterialStateProperty.all<Color>(Colors.blueGrey[800])),
+    //           child: Text('READ', style: TextStyle(color: Colors.white)),
+    //           onPressed: () {},
+    //         ),
+    //       ),
+    //     ),
+    //   );
+    // }
     if (characteristic.properties.write) {
       buttons.add(
-        ButtonTheme(
-          minWidth: 10,
-          height: 20,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 4),
-            child: RaisedButton(
-              child: Text('WRITE', style: TextStyle(color: Colors.white)),
+        Row(
+          children: <Widget>[
+            ElevatedButton(
+              child:
+                  Text('Send Message', style: TextStyle(color: Colors.white)),
+              style: ButtonStyle(
+                alignment: Alignment.center,
+                backgroundColor:
+                    MaterialStateProperty.all<Color>(Colors.blueGrey[800]),
+              ),
               onPressed: () async {
                 await showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        title: Text("Write"),
-                        content: Row(
-                          children: <Widget>[
-                            Expanded(
-                              child: TextField(
-                                controller: _writeController,
-                              ),
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Text("Type the message"),
+                      content: Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: TextField(
+                              controller: _writeController,
                             ),
-                          ],
-                        ),
-                        actions: <Widget>[
-                          FlatButton(
-                            child: Text("Send"),
-                            onPressed: () {
-                              characteristic.write(
-                                  utf8.encode(_writeController.value.text));
-                              Navigator.pop(context);
-                            },
-                          ),
-                          FlatButton(
-                            child: Text("Cancel"),
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
                           ),
                         ],
-                      );
-                    });
+                      ),
+                      actions: <Widget>[
+                        TextButton(
+                          child: Text("Cancel"),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                        TextButton(
+                          child: Text("Send"),
+                          onPressed: () {
+                            characteristic.write(
+                                utf8.encode(_writeController.value.text));
+                            Navigator.pop(context);
+                          },
+                        ),
+                      ],
+                    );
+                  },
+                );
               },
             ),
-          ),
+          ],
         ),
       );
     }
-    if (characteristic.properties.notify) {
-      buttons.add(
-        ButtonTheme(
-          minWidth: 10,
-          height: 20,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 4),
-            child: RaisedButton(
-              child: Text('NOTIFY', style: TextStyle(color: Colors.white)),
-              onPressed: () {},
-            ),
-          ),
-        ),
-      );
-    }
+    // if (characteristic.properties.notify) {
+    //   buttons.add(
+    //     ButtonTheme(
+    //       minWidth: 10,
+    //       height: 20,
+    //       child: Padding(
+    //         padding: const EdgeInsets.symmetric(horizontal: 4),
+    //         child: ElevatedButton(
+    //           child: Text('NOTIFY', style: TextStyle(color: Colors.white)),
+    //           onPressed: () {},
+    //         ),
+    //       ),
+    //     ),
+    //   );
+    // }
 
     return buttons;
   }
 
   ListView _buildConnectDeviceView() {
-    List<Container> containers = new List<Container>();
+    // List<Container> containers = new List<Container>();
+    List<Container> containers = [];
+    int count = 0;
 
     for (BluetoothService service in _services) {
-      List<Widget> characteristicsWidget = new List<Widget>();
+      // List<Widget> characteristicsWidget = new List<Widget>();
+      List<Widget> characteristicsWidget = [];
       for (BluetoothCharacteristic characteristic in service.characteristics) {
-        characteristic.value.listen((value) {
-          print(value);
-        });
+        // characteristic.value.listen((value) {
+        //   print(value);
+        // });
         characteristicsWidget.add(
           Align(
-            alignment: Alignment.centerLeft,
+            alignment: Alignment.center,
             child: Column(
               children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    Text(characteristic.uuid.toString(),
-                        style: TextStyle(fontWeight: FontWeight.bold)),
-                  ],
-                ),
+                // Row(
+                //   children: <Widget>[
+                //     Text(characteristic.uuid.toString(),
+                //         style: TextStyle(fontWeight: FontWeight.bold)),
+                //   ],
+                // ),
                 Row(
                   children: <Widget>[
                     ..._buildReadWriteNotifyButton(characteristic),
@@ -287,13 +300,38 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         );
       }
-      containers.add(
-        Container(
-          child: ExpansionTile(
-              title: Text(service.uuid.toString()),
-              children: characteristicsWidget),
-        ),
-      );
+      count++;
+      if (count == 3) {
+        containers.add(
+          Container(
+            // child: ExpansionTile(
+            //     title: Text(service.uuid.toString()),
+            //     children: characteristicsWidget),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      '\n\nTap the button to send a message\n\n',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: characteristicsWidget,
+                )
+              ],
+            ),
+          ),
+        );
+      }
     }
 
     return ListView(
